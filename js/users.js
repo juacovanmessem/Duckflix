9cd03e5af88e85a2f81d2086b811ec5f24d264ba
Joaquin Van Messem <vanmessemjoaquin@gmail.com>
2023-11-21 10:49:22 -0300

    correccion en los js individuales para las funciones de login

--- js/users.js
+++ js/users.js
@@ -9,18 +9,18 @@ const checkLogIn = () => {
     window.location.href = '../pages/login.html'
   }
 };
-// checkLogIn()
-// const showUser = async () => {
+checkLogIn()
+const showUser = async () => {
 
-//   let name = document.getElementById("userName"); 
-//   let id = localStorage.getItem ('id');
+  let name = document.getElementById("userName"); 
+  let id = localStorage.getItem ('id');
 
-//   const result = await fetch(`http://localhost:3000/users`);
-//   const info = await result.json();
-//   const userFind = info.find (e => e.id == id)
+  const result = await fetch(`http://localhost:3000/users`);
+  const info = await result.json();
+  const userFind = info.find (e => e.id == id)
 
-//   name.innerHTML = `<span class="nav-link active text-white"> Hola ${userFind.name}!</span>`
-// };
+  name.innerHTML = `<span class="nav-link active text-white"> Hola ${userFind.name}!</span>`
+};
 showUser()
 const adminButton = async () => {
   let div = document.getElementById("adminButton"); 
